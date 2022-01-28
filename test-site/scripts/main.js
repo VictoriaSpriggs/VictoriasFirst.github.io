3d4673e3b8346dadd932683ff6cca97aa9c5bd9e
VictoriaSpriggs <93560579+VictoriaSpriggs@users.noreply.github.com>
2022-01-28 13:09:29 -0500

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/lily.jpeg') {
+    myImage.setAttribute ('src','images/lily.jpeg');
+  } else {
+    myImage.setAttribute ('src','images/lily.jpeg');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'This is Lily, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'This is Lily, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
